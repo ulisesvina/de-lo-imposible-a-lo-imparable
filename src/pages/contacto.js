38995545bfeb13c98d6178ea4ea28aa783b9bddc
Ulises Viña <contacto@ulisesvina.me>
2023-06-20 17:54:42 -0600

    Cambios a contacto

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -1,15 +1,23 @@
 const Descarga = () => {
   return (
     <div className="flex text-2xl flex-col items-center justify-center h-screen w-screen">
-      <p className="text-4xl font-bold text-center mt-5">Contacto</p>
+      <p className="text-4xl font-bold text-center mt-5">¡NO ESTAS SOLX!</p>
       <div className="mt-10">
         <a
-          href="https://publuu.com/flip-book/169695/421071"
+          href="https://sicecon.fiscalia.gov.co/denuncia/LlenarFormulario "
           target="_blank"
           rel="noreferrer"
           className="bg-pink-500 hover:bg-pink-700 p-6 text-white font-bold rounded"
         >
-          Descarga aquí
+            Denuncia discriminación aquí
+        </a>
+        <a
+          href="https://bogota.gov.co/mi-ciudad/salud/bogota-el-distrito-ofrece-ayuda-psicologica-gratis-cuales-son"
+          target="_blank"
+          rel="noreferrer"
+          className="bg-pink-500 hover:bg-pink-700 p-6 text-white font-bold rounded ml-5"
+        >
+          Lineas de ayuda
         </a>
         <a
           href="mailto://contactoaloimparable@gmail.com"
