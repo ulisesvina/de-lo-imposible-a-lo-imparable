086c6a191f6148e20708d2852962c413e5f088c0
Ulises Viña <contacto@ulisesvina.me>
2023-06-20 17:57:54 -0600

    Agregue el titulo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="es">
+      <Head>
+        <title>De lo imposible a lo imparable</title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
